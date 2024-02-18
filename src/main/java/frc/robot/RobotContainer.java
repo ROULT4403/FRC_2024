7820617a3590273a772c871784e831f7547a9c10
Tejada06 <113258383+Tejada06@users.noreply.github.com>
2024-02-18 16:15:01 -0600

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,19 +15,40 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer
 {
   // The robot's subsystems and commands are defined here...
-  
+  private final TankDrive tankDrive = new TankDrive();
+  private final Climber climber = new Climber();
+  private final Wrist wrist = new Wrist();
+  private final Intake intake = new Intake();
+  private final Shooter shooter = new Shooter();
 
   // The driver's controllers are defined here...
-  
+  private final CommandXboxController chassisController(controllrsPort[0]);
+  private final CommandXboxController mechController(controllrsPort[1]);
+
+  private final Trigger x = mechController.x();
+  private final Trigger y = mechController.y();
+  private final Trigger shoot = mechController.rightTrigger();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
+  public RobotContainer()
+  {
+    tankDrive.setDefaultCommand(new DriveCommand(tankDrive, chassisController.getLeftY(), chassisController.getRightX()));
+    wrist.setDefaultCommand(new WristMove(wrist, mechController.getRightX()));
+    climber.setDefaultCommand(new ClimberMove(climber, mechController.getLeftY()));
+    
     // Configure the trigger bindings
     configureBindings();
   }
 
     /** Use this method to define your trigger->command mappings. */
-  private void configureBindings() {}
+  private void configureBindings()
+  {
+    x.onTrue(Commands.take);
+    y.onTrue(Commands.outtake);
+
+    shoot.onTrue(Commands.manualShoot);
+    shoot.onTrue(Commands.stopShoot);
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
