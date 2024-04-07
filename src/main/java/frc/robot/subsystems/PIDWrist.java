7f90a5ef3d7441625095ca27ce835498fa22f3e0
Roger079 <106194113+Roger079@users.noreply.github.com>
2024-04-07 17:37:13 -0600

    Added Motion Profiling for amp

--- src/main/java/frc/robot/subsystems/PIDWrist.java
+++ src/main/java/frc/robot/subsystems/PIDWrist.java
@@ -3,58 +3,50 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-import static edu.wpi.first.units.Units.Degree;
-import static edu.wpi.first.units.Units.Degrees;
+
 import static edu.wpi.first.units.Units.DegreesPerSecond;
-import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Volts;
-import static edu.wpi.first.units.Units.VoltsPerMeterPerSecond;
-import static edu.wpi.first.units.Units.Radian;
+
 import edu.wpi.first.wpilibj.Encoder;
 import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
-import static edu.wpi.first.units.Units.Rotation;
 import static edu.wpi.first.units.Units.Seconds;
-import static edu.wpi.first.units.Units.Volt;
 import static edu.wpi.first.units.MutableMeasure.mutable;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.MutableMeasure;
 import edu.wpi.first.units.Velocity;
 import edu.wpi.first.units.Voltage;
-import edu.wpi.first.units.*;
 
 
 import com.revrobotics.CANSparkMax;
 
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj.RobotController;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 import static frc.robot.Constants.ElectronicConstants.*;
-import static frc.robot.Constants.WristConstants.*;
 
-import javax.swing.text.Position;
 
-public class PIDWrist extends PIDSubsystem
+public class PIDWrist extends ProfiledPIDSubsystem
 {
   // The wrist's motor controller is defined here...
   private final CANSparkMax wrist = new CANSparkMax(3, neoMotorType);
-private final ArmFeedforward feedforward = new ArmFeedforward(0.37485, 7.0038, 0.11421, 1.0256);
+private static final ArmFeedforward feedforward = new ArmFeedforward(0.37485, 7.0038, 0.11421, 1.0256); 
   // The wrist's encoder is defined here...
 private final Encoder wristEncoder = new Encoder(2,3);  //MutableMeasure variables for sysID
 private final MutableMeasure<Voltage> m_appliedVoltage = mutable(Volts.of(0));
 private final MutableMeasure<Angle> m_Position = mutable(Radians.of(0));
 private final MutableMeasure<Velocity<Angle>> m_wristVelocity = mutable(DegreesPerSecond.of(0));
-
+private static final TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(2*Math.PI,2*Math.PI)
 
 private SysIdRoutine m_sysIdRoutine = 
 new SysIdRoutine(new SysIdRoutine.Config(Volts.of(.25).per(Seconds.of(1)), Volts.of(1), null), 
@@ -68,16 +60,15 @@ log -> {
   /** Creates a new WristPID. */
   public PIDWrist()
   {
-    super(new PIDController(62.192,0,0));
+    super(
+      new ProfiledPIDController(62.192,0,0, constraints));
     // The motor's mode is defined here...
     wrist.setIdleMode(neoBrakeMode);
     // The motor's inversion is defined here...
     wrist.setInverted(clockWise);
     // The encoders' distance per rotation are defined here...
     wristEncoder.setDistancePerPulse((2 * Math.PI)/8192);
-    getController().setTolerance(.005);
-    setSetpoint(-.36);
-  }
+  setGoal(VER CUAL ES EL GOAL ACA);  }
 
   
 
@@ -95,8 +86,9 @@ log -> {
     return m_sysIdRoutine.dynamic(direction);
   }
   @Override
-  public void useOutput(double output,double setpoint){
-    wrist.setVoltage(output);
+  public void useOutput(double output,TrapezoidProfile.State setpoint){
+    double FF = feedforward.calculate(setpoint.position, setpoint.velocity)
+    wrist.setVoltage(output + FF);
   }
   @Override
   public double getMeasurement()
